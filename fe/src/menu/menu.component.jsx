db9b3c83e21cca013a585b0d1ace3d1bed6a0ada
ddonvan <donovand9@mymacewan.ca>
2024-03-14 13:08:01 -0600

    started menu component

---
+++ fe/src/menu/menu.component.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export const MenuComponent = ({}) => {
+    const [menus, setMenu] = useState([]);
+
+    useEffect(() => {
+        const fetchMenus = async () => {
+            try {
+                const response = await axios.get('http://localhost:8000/menus');
+                setMenu(response.data);
+            } catch (error) {
+                console.error('Error fetching menus:', error);
+            }
+        };
+        fetchMenus();
+    }, []);
+
+    return (
+        <div className="Menu-card">
+            <ul>
+                {menus.map((menu,index) => (
+                    <p key={index}>
+                        {menu.item_name}: $ 
+                        {menu.price}
+                    </p>
+                ))}
+            </ul>
+
+        </div>
+    )
+}
+
+export default MenuComponent

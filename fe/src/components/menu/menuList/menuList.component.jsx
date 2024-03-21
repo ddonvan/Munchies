84b4e558424287c420e42c9ba9d519137d5f2318
nn4ncyy <118306106+nn4ncyy@users.noreply.github.com>
2024-03-21 13:28:34 -0600

    decrement/removed items fixed

--- fe/src/components/menu/menuList/menuList.component.jsx
+++ fe/src/components/menu/menuList/menuList.component.jsx
@@ -1,12 +1,30 @@
-// MenuList.component.jsx
-import React from "react";
+import React, { useState } from "react";
 import { Menu } from "../menuCard/menuCard.component";
 import Button from "react-bootstrap/Button";
 import './menuList.styles.css';
-import { useCart } from '../../../pages/CartPage/CartContext'; // Import useCart hook
+import { useCart } from '../../../pages/CartPage/CartContext'; 
 
 export const MenuList = ({ menus }) => {
-    const { addToCart } = useCart(); // Access addToCart function from the context
+    const { addToCart, removeFromCart } = useCart(); // Access addToCart and removeFromCart functions from the context
+    const [localCart, setLocalCart] = useState({}); // Local state for handling UI updates
+
+    const handleAddToCart = (itemId) => {
+        setLocalCart(prevLocalCart => ({
+            ...prevLocalCart,
+            [itemId]: (prevLocalCart[itemId] || 0) + 1
+        }));
+        addToCart(itemId); // Call addToCart function
+    };
+
+    const handleRemoveFromCart = (itemId) => {
+        if (localCart[itemId] > 0) {
+            setLocalCart(prevLocalCart => ({
+                ...prevLocalCart,
+                [itemId]: prevLocalCart[itemId] - 1
+            }));
+            removeFromCart(itemId); // Call removeFromCart function
+        }
+    };
 
     return (
         <div className="menuList">
@@ -14,7 +32,12 @@ export const MenuList = ({ menus }) => {
                 <div key={menu.item_name} className="menu-item">
                     <Menu key={menu.item_name} menu={menu}/>  
                     <div className="menu-item-actions">
-                        <Button onClick={() => addToCart(menu.item_id)} className="add-to-cart">Add to Cart</Button> {/* Call addToCart function on button click */}
+                        <div className="quantity-selector">
+                            <Button onClick={() => handleRemoveFromCart(menu._id)}>-</Button>
+                            <span>{localCart[menu._id] || 0}</span>
+                            <Button onClick={() => handleAddToCart(menu._id)}>+</Button>
+                        </div>
+                        <Button>Add to Cart</Button>
                     </div>
                 </div>
             ))}
